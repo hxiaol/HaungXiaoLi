b98dfe949047fd2b3a84bc601f0015660a167e92
落叶翻飞 <1912688797@qq.com>
2023-02-09 14:45:57 +0800

    add

---
+++ main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import App from './App'
+
+import basics from './pages/basics/home.vue'
+Vue.component('basics',basics)
+
+import components from './pages/component/home.vue'
+Vue.component('components',components)
+
+import my from './pages/my/home.vue'
+Vue.component('my',my)
+
+import cuCustom from './colorui/components/cu-custom.vue'
+Vue.component('cu-custom',cuCustom)
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
+
+ 
+
+
+
